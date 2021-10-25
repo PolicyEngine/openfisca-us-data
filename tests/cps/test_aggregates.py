7d69da40c144597f602c1dc6f44eddad0bdd310f
Max Ghenis <mghenis@gmail.com>
2021-10-25 09:23:53 -0700

    Re-add ACS tests

--- tests/cps/test_aggregates.py
+++ tests/cps/test_aggregates.py
@@ -28,14 +28,10 @@ def test_CPS_dataset_generates(year):
     CPS.generate(year)
 
 
-# Temporarily removed
-
-"""
 @pytest.mark.dependency(name="dataset")
 @pytest.mark.parametrize("year", ACS_YEARS)
 def test_ACS_dataset_generates(year):
     ACS.generate(year)
-"""
 
 
 @pytest.mark.dependency(depends=["dataset"])
@@ -46,17 +42,12 @@ def test_cps_openfisca_us_compatible(year):
     Microsimulation(dataset=CPS, year=year)
 
 
-# Temporarily removed
-
-
-"""
 @pytest.mark.dependency(depends=["dataset"])
 @pytest.mark.parametrize("year", ACS_YEARS)
 def test_acs_openfisca_us_compatible(year):
     from openfisca_us import Microsimulation
 
     Microsimulation(dataset=ACS, year=year)
-"""
 
 
 @pytest.mark.dependency(depends=["dataset"])
